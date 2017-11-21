b3797995c2084bf83b1af643779f00efa40c68f7
Josh Orndorff <admin@joshorndorff.com>
2017-11-21 01:17:59 -0500

    Sketched out assignment and brainstormed tests.

---
+++ README.md
@@ -0,0 +1,29 @@
+HashMap
+=======
+
+This exercise project implements a map in java according to the included `Map` interface. The particular implementation is built on a hashtable.
+
+Starter code for this repo is available at: [Pingry-DS/HashMap-StarterCode](https://github.com/Pingry-DS/HashMap-StarterCode/blob/master/Map.java)
+
+Implementation Details
+------------
+* No argument constructor
+* Constructor that takes a single int, number of bins
+* Random access is O(1)
+* Collisions are handled gracefully (See below)
+
+Tests
+-----
+To run the unit tests:
+```bash
+javac TestHashMap.java
+java <some runner here> TestHashMap
+```
+
+Collision Handling
+------------------
+Describe how your `HashMap` handles collisions.
+
+License
+-------
+Choose an existing license and explain why it's okay to use java library code with this project.
---
+++ TestHashMap.java
@@ -0,0 +1,41 @@
+// normal put
+// put with null key
+// put with null value
+// put with null key and value
+
+// get key not in map
+// get key in map
+// get null key (in map)
+// get null value
+
+// replace where key exists
+// replace where key doesn't exist
+// replace null key (not in map)
+// No size change after replace
+
+// remove key not in map
+// remove key in map
+// remove null key (in map)
+
+// containsKey true
+// containsKey false
+// containsKey null
+
+// size increases when putting new key
+// size does not grow when putting dupe key same value
+// size does not grow when putting dupe key new value
+// size is zero to start
+
+// isempty is true to start
+// isempty is false after adding
+// isempty is true after removing all
+// isempty is false after removing some
+
+// keySet for empty map
+// keySet for non empty map
+// keySet for map with null
+
+// putAll with target empty
+// putAll with m empty
+// putAll with neither empty
+// putAll with both empty
